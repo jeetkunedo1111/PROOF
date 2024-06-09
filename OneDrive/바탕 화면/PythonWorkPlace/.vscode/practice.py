105198bd372db3ac89517afb1a0b2af72eb33241
JunseongKim <65230333+jeetkunedo1111@users.noreply.github.com>
2024-06-09 14:25:17 +0900

    Initialize dino

---
+++ OneDrive/바탕 화면/PythonWorkPlace/.vscode/practice.py
@@ -0,0 +1,91 @@
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(3*(3+1))
+
+#문자열 자료형
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ"*9)
+#boolean
+#참/거짓
+print(5>10)
+print(5<10)
+print(True)
+print(not True)
+print(not False)
+print(not (5>10))
+#애완 동물을 소개해 주세요~
+
+animal = "강아지"
+name  = "연탄이"
+age = 4
+hobby = "낮잠"
+is_adult = age >=3 
+   
+
+# 
+
+# print("우리집"+animal+"의 이름은 연탄이에요")
+# print(name,"는",str(age)," 살이며, ",hobby+"을 아주 좋아해요")
+# print(name+"는 어른일까요?" + str(is_adult))
+
+#
+# range = station
+# station = "사당행"
+# staiton = "신도림"
+# staiton = "인천공항"
+
+# print((station), "열차가 들어오고 있습니다")
+# #연산자
+
+# print(1+1) #2
+# print(3-2) #1
+# print(5*2)
+# print(5%3)
+# print(10%3)
+# #//몫 구하는 법
+# print(10>3)
+# print(10<3)
+# print(4>=7)
+# print(4<=7)
+# print(5<=5)``
+# print(3 == 3)
+# print (1 != 3)
+# print(not(1 != 3)) #False
+# print((3> 0 )and (3< 5))
+# print((3>0)&(3<5))#True
+# print(3> 0 or 3<0)
+
+# from random import * 
+# date = randint(4, 28)
+# print("오프라인 스터디 모임 날짜는 매월"+ str(date) + " 일로 선정되엇습니다.")
+
+
+
+
+#문자열
+
+sentence = '나는 소년입니다'
+print(sentence)
+sentence2 = "파이썬은 쉬워요"
+
+print(sentence2)
+sentence3= """
+나는 소년이고,
+
+파이썬은 쉬워요
+"""
+print(sentence2)
+
+jumin = "990210-123456"
+
+print("성별:" + jumin[7])
+print("연:" + jumin[0:2])
+print("월:" +jumin[2:4])
+print("일:" + jumin[4:6])
+
+print("생년월일:" + jumin[:6])
+print("뒤 7자리:"+jumin[7:])
